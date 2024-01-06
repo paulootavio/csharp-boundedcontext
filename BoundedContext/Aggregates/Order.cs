d137b2c04ab73b4afb3f2aa068e9979f8f700b2c
paulo.otavio <paulo.otavio@esx.com.br>
2024-01-06 16:45:36 -0300

    new aggreggates

---
+++ BoundedContext/Aggregates/Order.cs
@@ -0,0 +1,16 @@
+﻿using BoundedContext.DomainModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BoundedContext.Aggregates
+{
+    public class Order
+    {
+        //change private acessors
+        public int Id { get; set; }
+        public List<Product> Products { get; set; }
+    }
+}
